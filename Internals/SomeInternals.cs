d828feee7154011efba60e3361cacbbe88eca24a
Arkadiusz Benedykt <arkadiusz@benedykt.net>
2018-04-25 14:43:13 +0200

    example nuget with multiple libs

---
+++ Internals/SomeInternals.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("NugetExample")]
+
+namespace Internals
+{
+    internal class SomeInternals
+    {
+        internal string ToUpperInvariant(string input)
+        {
+            return input.ToUpperInvariant();
+        }
+    }
+}
---
+++ NugetExample/StringTools.cs
@@ -0,0 +1,13 @@
+﻿using Internals;
+
+namespace NugetExample
+{
+    public class StringTools
+    {
+        public string ToUpper(string input)
+        {
+            var i = new SomeInternals();
+            return i.ToUpperInvariant(input);
+        }
+    }
+}
